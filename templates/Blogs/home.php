d02b1e6d112185d4c151a63828d949051d6f1479
Matheus Fantoni <matheusfantoni14@hotmail.com>
2020-08-26 16:20:20 -0300

    truncating text

--- templates/Blogs/home.php
+++ templates/Blogs/home.php
@@ -24,12 +24,24 @@
 
                   
                   
-                  <a href="#" class="list-group-item list-group-item-action flex-column mb-2">
+                  <a href=<?= $this->Url->build(['controller' => 'Blogs', 'action' => 'view', $article->id]) ?> class="list-group-item list-group-item-action flex-column mb-2">
                     <div class="d-flex w-100 justify-content-between">
                       <h5 class="mb-1"><?= $article-> title ?></h5>
                       <small>3 days ago</small>
                     </div>
-                    <p class="mb-1"><?= $article-> details ?></p>
+                    <p class="mb-1">
+                    <?=
+                     $this->Text->truncate(
+                            $article-> details,
+                            280,
+                            [
+                                'ellipsis' => '...',
+                                'exact' => true
+                            ]
+                        );
+                    ?>
+                    
+                    </p>
                     
                   </a>
 
